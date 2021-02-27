ef47263dd2a2e6034a15ec9c1b7b12cf9cd894da
DESKTOP-M0IPLA9\Master_CRAN <cristeaanastasia13@gmail.com>
2021-02-27 14:27:26 +0100

    no message

---
+++ Assets/Scripts/Hazards/magnet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magnet : MonoBehaviour
+{
+    public float speed;
+
+    private void OnTriggerStay(Collider other)
+    {
+        float forceFactor = speed * Time.deltaTime;
+        var player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<Rigidbody>().AddForce((this.transform.position - player.transform.position) * speed * Time.smoothDeltaTime);
+        //player.transform.position = Vector3.MoveTowards(player.transform.position, -this.transform.position, forceFactor);
+    }
+
+}
+
