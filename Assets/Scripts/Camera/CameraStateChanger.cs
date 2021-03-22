0df50907539578cb09dd1040846de7abf22036b0
CaveTalesZ <57946278+CaveTalesZ@users.noreply.github.com>
2021-03-22 05:07:38 +0100

    Implemented Cinemachine with self-made script to adapt to player positions, worked out camera positions, tracks, and cuts for the first ~half of the track

---
+++ Assets/Scripts/Camera/CameraStateChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraStateChanger : MonoBehaviour
+{
+    private Collider collider;
+    private CinemachineBrain mainBrain;
+    private CinemachineVirtualCamera mainCam;
+    public CinemachineVirtualCamera newCam;
+    public bool firstOnly = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainBrain = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CinemachineBrain>();
+        collider = gameObject.GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (firstOnly)
+            {
+                if(other.GetComponent<BaseAI>().position == 1)
+                {
+                    mainBrain.ActiveVirtualCamera.Priority = 0;
+                    newCam.Priority = 1;
+                }
+            }
+            else
+            {
+                mainBrain.ActiveVirtualCamera.Priority = 0;
+                newCam.Priority = 1;
+            }
+        }
+    }
+}
