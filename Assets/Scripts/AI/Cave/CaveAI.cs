6b2a9078947f6107ee9613a50db135a99d42410f
CaveTalesZ <57946278+CaveTalesZ@users.noreply.github.com>
2021-03-25 23:10:33 +0100

    Improved CaveAI with raytraced paths

--- Assets/Scripts/AI/Cave/CaveAI.cs
+++ Assets/Scripts/AI/Cave/CaveAI.cs
@@ -15,12 +15,13 @@ public class CaveAI : MonoBehaviour
     public int nodesAhead = 3;
     [Tooltip("How close a node should be")]
     public float confirmationDistance = 5.0f;
+    // Literally all the nodes in the level
     private List<Node> allNodes = new List<Node>();
     // The current node being travelled towards
-    public Vector3 currentDestination;
+    private Vector3 currentDestination;
     private float currentDistance;
-    public float respawnDistance = 10.0f;
-    public Node currentNode;
+    // The node being checked ahead of
+    private Node currentNode;
 
     [Header("Movement")]
     public float maxSpeed = 1.0f;
@@ -30,7 +31,7 @@ public class CaveAI : MonoBehaviour
     public float precision;
     [Range(2.0f, 180.0f), Tooltip("How wide should the cone be when checking the nearest node")]
     public float coneSpread;
-    [Range(1.0f, 20.0f), Tooltip("How far rays should be cast")]
+    [Range(10f, 200f), Tooltip("How far rays should be cast")]
     public float rayDistance;
     [Tooltip("Which layers should be checked for walls & pathing")]
     public LayerMask walls;
@@ -58,21 +59,30 @@ public class CaveAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FindCurrentNode();
+        currentDestination = FindLongest(FindFurthestNode().transform.position);
+        SteerTowards(currentDestination, maxSpeed);
+        baseAI.GetDirection();
+    }
+
+    public void FindCurrentNode()
+    {
+        // Increases to the next node if near a node
         if (Vector3.Distance(transform.position, currentNode.transform.position) < confirmationDistance)
         {
             currentNode = currentNode.nextNodes[0];
         }
-        //currentDistance = Mathf.Infinity;
-        //if(Vector3.Distance)
+
+        // Finds the nearest node and sets it as the current node if there is no clear line to the destination
         if (Physics.Linecast(transform.position, currentDestination, walls))
         {
             float closestDistance = Mathf.Infinity;
-            Node possibleNode = new Node();
+            Node possibleNode = null;
             // The car has probably died and so it can't find a line to the destination node
-            foreach(Node tryNode in allNodes)
+            foreach (Node tryNode in allNodes)
             {
                 float tryDistance = Vector3.Distance(tryNode.transform.position, transform.position);
-                if(tryDistance < closestDistance)
+                if (tryDistance < closestDistance)
                 {
                     closestDistance = tryDistance;
                     possibleNode = tryNode;
@@ -80,14 +90,9 @@ public class CaveAI : MonoBehaviour
             }
             currentNode = possibleNode;
         }
-        currentDestination = FindFurthest().transform.position;
-        SteerTowards(currentDestination, maxSpeed);
-        baseAI.GetDirection();
-
-        
     }
 
-    public Node FindFurthest()
+    public Node FindFurthestNode()
     {
         Node checkNode = currentNode;
         Node furthestNode = checkNode;
@@ -110,17 +115,51 @@ public class CaveAI : MonoBehaviour
     public void SteerTowards(Vector3 direction, float speed)
     {
         Vector3 dir = transform.InverseTransformDirection((direction - transform.position).normalized);
-        baseAI.SetDirection(new Vector2(dir.x, 1));
+        baseAI.SetDirection(new Vector2(dir.x, speed));
     }
 
-    //public Vector2 FindLongest()
-    //{
-    //    Vector2 coneCenter = currentNode
-    //    for (float i = 0.0f; i < coneSpread; i += precision)
-    //    {
-    //        Vector2 direction;
-    //        Physics.Raycast(transform.position,
-    //                        new Vector3(direction.x, transform.position.y, direction.y));
-    //    }
-    //}
+    public Vector3 FindLongest(Vector3 coneCenter)
+    {
+        Vector3 baseDirection = (coneCenter - transform.position).normalized;
+        float longestDistance = 0.0f;
+        Vector2 outputDirection = new Vector2();
+        List<Vector3> debugRayLines = new List<Vector3>();
+        Vector3 outputDestination = new Vector3();
+
+        for (float i = 0.0f; i < coneSpread * 2; i += precision)
+        {
+            Vector3 direction = Quaternion.AngleAxis(i - coneSpread, transform.up) * baseDirection;
+            RaycastHit hit;
+            Physics.Raycast(transform.position,
+                            direction, 
+                            out hit,
+                            rayDistance,
+                            walls);
+            if(hit.distance > longestDistance)
+            {
+                longestDistance = hit.distance;
+                outputDirection = direction;
+                outputDestination = hit.point;
+            }
+            if (debug)
+            {
+                debugRayLines.Add(direction);
+            }
+        }
+        if (debug)
+        {
+            foreach (Vector3 ray in debugRayLines)
+            {
+                if ((Vector2)ray == outputDirection)
+                {
+                    Debug.DrawRay(transform.position, ray * rayDistance, Color.yellow);
+                }
+                else
+                {
+                    Debug.DrawRay(transform.position, ray * rayDistance, Color.cyan);
+                }
+            }
+        }
+        return outputDestination;
+    }
 }
